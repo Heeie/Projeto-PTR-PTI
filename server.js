ab74695ec0fb0817f65d6075304e74701ecd833b
fc57576 <fc57576@alunos.fc.ul.pt>
2025-07-11 19:12:49 +0100

    utilizadorRoutes

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.use('/Images', express.static(path.join(__dirname, 'public/Images')));
 
 // Rotas organizadas
 app.use('/api/equipamentos', equipamentoRoutes);
-app.use('/api/utilizadores', utilizadorRoutes);
+app.use('/api', utilizadorRoutes);
 app.use('/api/catalogos', catalogoRoutes);
 app.use('/api/categorias', categoriaRoutes);
 app.use('/api/tipos', tipoRoutes);
