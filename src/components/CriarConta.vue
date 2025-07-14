1eccf50e6e137f550600605f0ecb49e0763f0449
fc57576 <fc57576@alunos.fc.ul.pt>
2025-07-14 21:18:32 +0100

    caminhos

--- src/components/CriarConta.vue
+++ src/components/CriarConta.vue
@@ -172,7 +172,7 @@ export default {
       this.errorMessage = "";
 
       try {
-        const response = await fetch("/api/criar", {
+        const response = await fetch("/utilizadores/criar", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
