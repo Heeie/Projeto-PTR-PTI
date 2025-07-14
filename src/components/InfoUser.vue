2f95ca4a31f92d055aac94dea58f2c7140e48266
fc57576 <fc57576@alunos.fc.ul.pt>
2025-07-14 19:31:40 +0100

    alteração caminho utilizadores

--- src/components/InfoUser.vue
+++ src/components/InfoUser.vue
@@ -131,7 +131,7 @@
 </template>
 
 <script setup>
-import { ref, onMounted, computed } from 'vue';
+import { ref, onMounted } from 'vue';
 import axios from 'axios';
 import { useRouter } from 'vue-router';
 
